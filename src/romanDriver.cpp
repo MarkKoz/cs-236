18d910665e1c8824a6ddb4da6d49a8cb0f993314
MarkKoz <KozlovMark@gmail.com>
2021-02-19 19:57:11 -0800

    Create main input loop

--- src/romanDriver.cpp
+++ src/romanDriver.cpp
@@ -1,3 +1,31 @@
+#include <string>
+#include <iostream>
+
 int main() {
+    bool stop = false;
+    std::string selection;
+
+    while (!stop) {
+        std::cout << "Enter a roman numeral: ";
+        std::getline(std::cin, selection);
+
+        std::cout << "The equivalent of the roman numeral is \n";
+
+        std::cout << "Try another number (y/n)? ";
+        while (true) {
+            std::getline(std::cin, selection);
+
+            if (selection == "y" || selection == "Y") {
+                break;
+            } else if (selection == "n" || selection == "N") {
+                std::cout << "Thank you!\n";
+                stop = true;
+                break;
+            } else  {
+                std::cerr << "Unknown value, try again: ";
+            }
+        }
+    }
+
     return 0;
 }
