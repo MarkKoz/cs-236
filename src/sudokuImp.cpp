d041c5b4dc7e789956d3b0ea53576d1a029e5114
MarkKoz <KozlovMark@gmail.com>
2021-02-25 15:36:14 -0800

    Add stub implementations for the sudoku class

---
+++ src/sudokuImp.cpp
@@ -0,0 +1,37 @@
+#include "sudoku.h"
+
+sudoku::sudoku() : grid_ {} { }
+
+sudoku::sudoku(const grid_t& grid) : grid_ {grid} { }
+
+void sudoku::print() const { }
+
+bool sudoku::solve()
+{
+    return false;
+}
+
+bool sudoku::can_fill(const index_t row, const index_t column, const cell_t value) const
+{
+    return false;
+}
+
+bool sudoku::in_column(const index_t column, const cell_t value) const
+{
+    return false;
+}
+
+bool sudoku::in_row(const index_t row, const cell_t value) const
+{
+    return false;
+}
+
+bool sudoku::in_subgrid(const index_t row, const index_t column, const cell_t value) const
+{
+    return false;
+}
+
+bool sudoku::is_empty(const index_t row, const index_t column) const
+{
+    return false;
+}
