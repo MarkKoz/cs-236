e38a0d74ecf148b8c9d4919c91ed0fb0fda23356
MarkKoz <KozlovMark@gmail.com>
2021-03-07 18:40:50 -0800

    Implement driver

--- src/sudokuDriver.cpp
+++ src/sudokuDriver.cpp
@@ -1,5 +1,27 @@
 #include "sudoku.h"
 
-int main() {
+int main()
+{
+    std::cout << "Enter a partially filled sudoku grid: ";
+
+    sudoku::grid_t grid {};
+    for (auto& row : grid) {
+        for (unsigned int& cell : row) {
+            // Fill in the grid with the user's input.
+            // Assume the value for each cell is valid.
+            std::cin >> std::ws >> cell;
+        }
+    }
+
+    sudoku puzzle(grid);
+    std::cout << "\nGrid before solution:\n\n" << puzzle << '\n';
+
+    // Try to solve the puzzle.
+    if (puzzle.solve()) {
+        std::cout << "Solution:------------------\n\n" << puzzle;
+    } else {
+        std::cout << "No solutions.\n";
+    }
+
     return 0;
 }
