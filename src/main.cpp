467b7e5b3bc85c24a5c82313ec6759e3260a7340
MarkKoz <KozlovMark@gmail.com>
2021-04-11 11:18:26 -0700

    Insert user's input into the tree and display the tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include "rb_tree.h"
 
+#include <iostream>
+
 int main()
 {
+    rb_tree<int> tree;
+
+    std::cout << "Enter a space-delimited sequence of integers to insert into a red-black tree:\n";
+    std::string input_line;
+    std::getline(std::cin, input_line);
+
+    int input_int = 0;
+    std::istringstream input_stream(input_line);
+
+    while (input_stream >> input_int) {
+        tree.insert(input_int);
+    }
+
+    std::cout << "Final balanced red-black tree:" << tree;
     return 0;
 }
