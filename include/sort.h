bdae9bcffde4c8e2248718ea5f4bcaaee1b5bc3d
MarkKoz <KozlovMark@gmail.com>
2021-04-25 19:02:45 -0700

    Add documentation and rename "array" params to "vector"

--- include/sort.h
+++ include/sort.h
@@ -4,29 +4,81 @@
 #include <cstddef>
 #include <vector>
 
+/**
+ * @brief The type of algorithm to use to select a pivot for a quick sort.
+ */
 enum PivotSelection
 {
+    // Select the middle element of the sub-list.
     middle,
-    median
+    // Select the median of the start, end, and middle of the sub-list.
+    median,
 };
 
-void insertion_sort(std::vector<int>& array, std::size_t start, std::size_t end);
+/**
+ * @brief Perform an insertion sort on the given range of elements in a vector of integers.
+ *
+ * @param vector The vector to sort.
+ * @param start The index of the first item in the sub-vector to sort.
+ * @param end The index of the last item in the sub-vector to sort.
+ */
+void insertion_sort(std::vector<int>& vector, std::size_t start, std::size_t end);
 
-std::size_t partition(std::vector<int>& array, std::size_t start, std::size_t end);
+/**
+ * @brief Partition a vector of integers around a pivot point for a quick sort.
+ *
+ * Partition items such that all items to the left are smaller than the pivot and all items to the
+ * right are greater than the pivot.
+ *
+ * Use the item at the @c start index as the pivot.
+ *
+ * @param vector The vector to partition.
+ * @param start The index of the first item in the sub-vector to partition.
+ * @param end The index of the last item in the sub-vector to partition.
+ *
+ * @return The index of the pivot.
+ */
+std::size_t partition(std::vector<int>& vector, std::size_t start, std::size_t end);
 
+/**
+ * @brief Quick sort a sub-vector of integer..
+ *
+ * @param vector The vector to sort.
+ * @param start The index of the first item in the sub-vector to sort.
+ * @param end The index of the last item in the sub-vector to sort.
+ * @param pivot_selection The way the pivot should be chosen.
+ * @param use_insertion_sort @c true to use insertion sort on sub-vectors with < 20 items.
+ */
 void quick_sort(
-    std::vector<int>& array,
+    std::vector<int>& vector,
     std::size_t start,
     std::size_t end,
     PivotSelection pivot_selection,
     bool use_insertion_sort);
 
+/**
+ * @brief Quick sort a vector of integer.
+ *
+ * @param vector The vector to sort.
+ * @param pivot_selection The way the pivot should be chosen.
+ * @param use_insertion_sort @c true to use insertion sort on sub-vectors with < 20 items.
+ */
 void quick_sort(
-    std::vector<int>& array,
+    std::vector<int>& vector,
     PivotSelection pivot_selection = PivotSelection::middle,
     bool use_insertion_sort = false);
 
+/**
+ * @brief Selects the pivot to use to sort the given vector.
+ *
+ * Move the pivot item to the @c start index by swapping it with the element at @c start.
+ *
+ * @param vector The vector being sorted.
+ * @param start The index of the first item in the sub-vector being sorted.
+ * @param end The index of the last item in the sub-vector being sorted.
+ * @param pivot_selection The way the pivot should be chosen.
+ */
 void select_pivot(
-    std::vector<int>& array, std::size_t start, std::size_t end, PivotSelection pivot_selection);
+    std::vector<int>& vector, std::size_t start, std::size_t end, PivotSelection pivot_selection);
 
 #endif
--- src/sort.cpp
+++ src/sort.cpp
@@ -3,40 +3,40 @@
 #include <cassert>
 #include <stdexcept>
 
-void insertion_sort(std::vector<int>& array, std::size_t start, std::size_t end)
+void insertion_sort(std::vector<int>& vector, std::size_t start, std::size_t end)
 {
     // Start at 1 because the 0th element is already considered sorted.
     for (std::size_t unsorted_i = start + 1; unsorted_i <= end; ++unsorted_i) {
-        int item = array[unsorted_i];
+        int item = vector[unsorted_i];
         std::size_t sorted_i = unsorted_i; // One past the end of the sorted sub-list.
 
         // Start at the end of the sorted sublist and go backwards until the sorted position for
         // `item` is found or the start of the sub-list is reached.
-        for (; sorted_i > start && array[sorted_i - 1] > item; --sorted_i) {
+        for (; sorted_i > start && vector[sorted_i - 1] > item; --sorted_i) {
             // Move back by one position to make space for `item` in the sorted sub-list.
-            array[sorted_i] = array[sorted_i - 1];
+            vector[sorted_i] = vector[sorted_i - 1];
         }
 
         // Insert the `item` into its correct position within the sorted sub-list.
-        array[sorted_i] = item;
+        vector[sorted_i] = item;
     }
 }
 
-std::size_t partition(std::vector<int>& array, std::size_t start, std::size_t end)
+std::size_t partition(std::vector<int>& vector, std::size_t start, std::size_t end)
 {
-    int pivot = array[start];
+    int pivot = vector[start];
     std::size_t low = start + 1;
     std::size_t high = end;
 
     // Keep swapping values on either side until the indices for the two sides meet.
     while (true) {
         // Move from left to right until low is greater than the pivot.
-        while (low <= high && array[low] <= pivot) {
+        while (low <= high && vector[low] <= pivot) {
             ++low;
         }
 
         // Move from right to left until high is less than or equal to the pivot.
-        while (low <= high && array[high] > pivot) {
+        while (low <= high && vector[high] > pivot) {
             --high;
         }
 
@@ -45,18 +45,18 @@ std::size_t partition(std::vector<int>& array, std::size_t start, std::size_t en
         // Swapping them means the greater value will be moved ahead of the pivot and the smaller
         // value will be moved before the pivot.
         if (high > low) {
-            std::swap(array[low], array[high]);
+            std::swap(vector[low], vector[high]);
         } else {
             break;
         }
     }
 
-    std::swap(array[start], array[high]);
+    std::swap(vector[start], vector[high]);
     return high;
 }
 
 void quick_sort(
-    std::vector<int>& array,
+    std::vector<int>& vector,
     std::size_t start,
     std::size_t end,
     PivotSelection pivot_selection,
@@ -64,28 +64,28 @@ void quick_sort(
 {
     if (end > start) {
         if (use_insertion_sort && end - start < 19) {
-            insertion_sort(array, start, end);
+            insertion_sort(vector, start, end);
         } else {
-            select_pivot(array, start, end, pivot_selection);
-            std::size_t pivot = partition(array, start, end);
+            select_pivot(vector, start, end, pivot_selection);
+            std::size_t pivot = partition(vector, start, end);
 
             // Avoid overflow since pivot is unsigned.
             if (pivot > 0) {
-                quick_sort(array, start, pivot - 1, pivot_selection, use_insertion_sort);
+                quick_sort(vector, start, pivot - 1, pivot_selection, use_insertion_sort);
             }
 
-            quick_sort(array, pivot + 1, end, pivot_selection, use_insertion_sort);
+            quick_sort(vector, pivot + 1, end, pivot_selection, use_insertion_sort);
         }
     }
 }
 
-void quick_sort(std::vector<int>& array, PivotSelection pivot_selection, bool use_insertion_sort)
+void quick_sort(std::vector<int>& vector, PivotSelection pivot_selection, bool use_insertion_sort)
 {
-    quick_sort(array, 0, array.size() - 1, pivot_selection, use_insertion_sort);
+    quick_sort(vector, 0, vector.size() - 1, pivot_selection, use_insertion_sort);
 }
 
 void select_pivot(
-    std::vector<int>& array, std::size_t start, std::size_t end, PivotSelection pivot_selection)
+    std::vector<int>& vector, std::size_t start, std::size_t end, PivotSelection pivot_selection)
 {
     std::size_t pivot_index = start + ((end - start) / 2); // Calculate the middle.
     assert(pivot_index >= start);
@@ -93,11 +93,11 @@ void select_pivot(
 
     if (pivot_selection == PivotSelection::median) {
         // Quickly find the index for the median of the start, end, and middle.
-        int delta = array[start] - array[end]; // Positive if start is greater.
+        int delta = vector[start] - vector[end]; // Positive if start is greater.
 
-        if (delta * (array[end] - array[pivot_index]) > 0) {
+        if (delta * (vector[end] - vector[pivot_index]) > 0) {
             pivot_index = end;
-        } else if (delta * (array[start] - array[pivot_index]) < 0) {
+        } else if (delta * (vector[start] - vector[pivot_index]) < 0) {
             pivot_index = start;
         }
     } else if (pivot_selection != PivotSelection::middle) {
@@ -105,5 +105,5 @@ void select_pivot(
     }
 
     // Swap the pivot to the start.
-    std::swap(array[start], array[pivot_index]);
+    std::swap(vector[start], vector[pivot_index]);
 }
