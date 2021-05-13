939353a1b9efb3ae8566b5de5a45bae496f7d91a
MarkKoz <KozlovMark@gmail.com>
2021-05-12 18:28:15 -0700

    Implement algorithm to find the shortest path

--- include/graph.h
+++ include/graph.h
@@ -9,9 +9,11 @@ struct vertex
 {
     std::string name;
     std::vector<std::size_t> neighbours;
-    std::size_t prev = 0;
+    std::size_t previous = 0;
 
     friend std::ostream& operator<<(std::ostream& stream, const vertex& vertex);
 };
+
+void find_shortest_path(std::vector<vertex>& vertices, std::size_t start, std::size_t end);
 
 #endif
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,12 +1,52 @@
 #include "graph.h"
 
 #include <ostream>
+#include <set>
 
-std::ostream& operator<<(std::ostream& stream, const vertex& vertex) {
+std::ostream& operator<<(std::ostream& stream, const vertex& vertex)
+{
     stream << vertex.name << " --";
     for (const auto neighbour : vertex.neighbours) {
         stream << ' ' << neighbour;
     }
 
     return stream;
+}
+
+void find_shortest_path(
+    std::vector<vertex>& vertices, const std::size_t start, const std::size_t end)
+{
+    std::size_t length = 0;
+    std::size_t shortest = vertices.size() + 1; // Initial value is intentionally impossible.
+    std::set<std::size_t> visited;
+
+    // The lambda needs a reference to itself in order to recurse.
+    const auto dfs = [&](const auto& self, const std::size_t index) -> void {
+        ++length;
+
+        if (length > shortest) {
+            // Discard the current path because it's longer than the shortest known path.
+            return;
+        }
+
+        if (index == end) {
+            // The end has been reached; save the path as the new shortest path.
+            shortest = length;
+            return;
+        }
+
+        visited.insert(index); // Mark the node as visited.
+
+        // Visit all unvisited neighbours.
+        for (const auto neighbour : vertices[index].neighbours) {
+            if (visited.count(neighbour) == 0) {
+                self(self, neighbour);
+                vertices[neighbour].previous = index;
+            }
+        }
+
+        --length;
+    };
+
+    dfs(dfs, start);
 }
