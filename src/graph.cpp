727ff8910bf728ea833359011170370b426632c6
MarkKoz <KozlovMark@gmail.com>
2021-05-22 13:24:30 -0700

    Sort edges

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,6 +1,16 @@
 #include "graph.h"
 
+#include <algorithm>
+
 std::vector<edge> kruskals_mst(std::vector<edge>& edges)
 {
+    // Sort edges by weight in ascending order.
+    const auto comp = [](const edge& edge_1, const edge& edge_2) -> bool {
+        return edge_1.weight < edge_2.weight;
+    };
+    std::sort(edges.begin(), edges.end(), comp);
+
+
+
     return {};
 }
