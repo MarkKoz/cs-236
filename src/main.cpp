263773dcc8897b429b1337534cffad224db905a1
MarkKoz <KozlovMark@gmail.com>
2021-05-22 13:20:45 -0700

    Create the edge vector and add some of the prints in the main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,27 @@
+#include "graph.h"
+
+#include <iostream>
+
 int main()
 {
+    std::vector<edge> edges {{
+        {'A', 'B', 6},
+        {'B', 'C', 10},
+        {'B', 'F', 8},
+        {'C', 'D', 8},
+        {'C', 'E', 10},
+        {'D', 'E', 8},
+        {'E', 'F', 12},
+        {'F', 'A', 5},
+        {'G', 'B', 7},
+        {'G', 'C', 5},
+        {'G', 'E', 7},
+        {'G', 'F', 7},
+    }};
+    std::vector<edge> mst = kruskals_mst(edges);
+
+    std::cout << "Finding MST using Kruskal's Algorithm\nEdges of MST are\n";
+    std::cout << "Weight of MST is " << '\n';
+
     return 0;
 }
