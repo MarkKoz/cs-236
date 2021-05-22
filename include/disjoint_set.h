c7afeb1d96df7c967264e967e157351e5b527a2a
MarkKoz <KozlovMark@gmail.com>
2021-05-22 14:30:51 -0700

    Add documentation

--- include/disjoint_set.h
+++ include/disjoint_set.h
@@ -3,10 +3,22 @@
 
 #include <unordered_map>
 
+/**
+ * @brief Stores a collection of disjoint sets.
+ *
+ * @tparam T The type of the elements stored in the sets.
+ */
 template<class T>
 class disjoint_set
 {
 public:
+    /**
+     * @brief Find the root element of the set to which the given element belongs.
+     *
+     * @param element The element for which to find the root.
+     *
+     * @return The root element of the set to which the given element belongs.
+     */
     T find(T element)
     {
         if (parents_[element] == element) {
@@ -16,11 +28,22 @@ public:
         return find(parents_[element]);
     }
 
+    /**
+     * @brief Make a new set containing a new element.
+     *
+     * @param element The new element to place in the new set.
+     */
     void make_set(T element)
     {
         parents_[element] = element;
     }
 
+    /**
+     * @brief Merge the two sets which contain the given elements.
+     *
+     * @param a The element in the set to merge.
+     * @param b The element in the other set to merge.
+     */
     void unite(T a, T b)
     {
         T parent_a = find(a);
--- include/graph.h
+++ include/graph.h
@@ -3,12 +3,33 @@
 
 #include <vector>
 
+/**
+ * @brief An edge of a weighted, undirected graph.
+ */
 struct edge {
+    /**
+     * @brief One of the two vertices joined by the edge.
+     */
     char endpoint_1;
+
+    /**
+     * @brief One of the two vertices joined by the edge.
+     */
     char endpoint_2;
+
+    /**
+     * @brief The weight of the edge.
+     */
     std::size_t weight;
 };
 
+/**
+ * @brief Find the edges of the minimum spanning tree (MST) of a graph using Kruskal's algorithm.
+ *
+ * @param edges The edges of a connected, undirected, weighted graph for which to find the MST.
+ *
+ * @return The edges of the minimum spanning tree.
+ */
 std::vector<edge> kruskals_mst(std::vector<edge>& edges);
 
 #endif
